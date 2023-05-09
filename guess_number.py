296a42ac1237c418c5fbeaa13e5a7006cf58f36d
davidcxpardede <davidpardede40@gmail.com>
2023-05-09 17:27:19 +0700

    Computer Guess

--- guess_number.py
+++ guess_number.py
@@ -12,4 +12,29 @@ def guess(x):
             print("Try a lower number!")
     print("Congratulations, you have guessed the number!")
     
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #Could also be "high"
+        feedback = input(f"Is {guess} too high(H), too low(L), or correct(C)?").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print("The computer have guessed your number!")
+
+#Unccomment the corresponding code to play the guessing game
+
+#Defining the Range
+range = 10  #Change the value as your wish (integer).
+
+#User Guessing the Number
+#guess(range)
+
+#Computer Guessing the Number
+#computer_guess(range)
