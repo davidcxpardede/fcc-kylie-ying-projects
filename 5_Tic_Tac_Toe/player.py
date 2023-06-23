09d3241972b48a49fae535a04640843cb9dc7b20
davidcxpardede <davidpardede40@gmail.com>
2023-06-23 14:29:40 +0700

    P

--- 5_Tic_Tac_Toe/player.py
+++ 5_Tic_Tac_Toe/player.py
@@ -14,11 +14,24 @@ class RandomComputerPlayer(Player):
         super()._init_letter()
         
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
     
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
     
     def get_move(self, game):
-        pass
+       valid_square = False
+       val = None
+       while not valid_square:
+           square = input(self.letter + '\'s turn. Input move (0-9): ')
+           #Check that this is a correct value by trying to cast it into an integer.
+           #If it is not an integer or not available on the board, it is invalid
+           try:
+               val = int(square)
+               if val not in game.available_moves():
+                   raise ValueError
+               valid_square = True
+        
+    
